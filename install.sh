529e8dd2793f7498e2d9b660a457b864bcca21cf
umayanga <umayangag@gmail.com>
2021-06-29 19:51:57 +0530

    endpoint added

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ sudo pip3 install virtualenv
 virtualenv -p /usr/bin/python3.8 venv
 source venv/bin/activate
 pip install --upgrade pip
+pip install flask flask_restful waitress pytesseract requests
+sudo apt install -y tesseract-ocr
+sudo apt install -y libtesseract-dev
--- ocr_server.py
+++ ocr_server.py
@@ -1,48 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from nltk.tag import StanfordNERTagger
-from nltk.tokenize import word_tokenize
+import io
+import requests
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask import request
 from waitress import serve
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
 
 app = Flask(__name__)
 api = Api(app)
 
-st = StanfordNERTagger('stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz',
-                       'stanford-ner/stanford-ner.jar',
-                       encoding='utf-8')
 
-
-# sort the NER result to merge entities of same class together
-def sort_entities(classified_text):
-    classified_text.append([" ", "O"])
-    sorted_array = []
-    previous_word = ""
-    previous_class = ""
-
-    for current_word, current_class in classified_text:
-        if current_class == previous_class:
-            previous_word += " " + current_word
-        else:
-            if [previous_word, previous_class] not in sorted_array and previous_word != "" and previous_class != "O":
-                sorted_array.append([previous_word, previous_class])
-            previous_word = current_word
-            previous_class = current_class
-    return sorted_array
-
-
-class NER(Resource):
-    def post(self):
-        if request.get_json() is None:
+class OCR(Resource):
+    def get(self):
+        url = request.args.get('url')
+        if url is None or url == "":
             return {"error": "invalid input"}, 500
-
-        text = request.get_json().replace("\n", "\n<<enter>> ")
-        tokenized_text = word_tokenize(text)
-        classified_text = st.tag(tokenized_text)
-
-        return sort_entities(classified_text), 200
+        response = requests.get(url)
+        img = Image.open(io.BytesIO(response.content))
+        return pytesseract.image_to_string(img)
 
 
 class HealthCheck(Resource):
@@ -50,6 +31,6 @@ class HealthCheck(Resource):
         return "health is okay", 200
 
 
-api.add_resource(NER, "/classify")
+api.add_resource(OCR, "/extract")
 api.add_resource(HealthCheck, "/health")
-serve(app, port=8080)  # Change port number for production
+serve(app, port=8081)  # Change port number for production
